fa4bddc3b76acf24a5e297efd5bc045e71c3e97b
syo00 <namamugi.namagome.namayasai@gmail.com>
2016-09-05 16:41:56 +0000

    fix issues

--- .profile.d/buildpack-boot.sh
+++ .profile.d/buildpack-boot.sh
@@ -1,4 +1,5 @@
 export PATH="$PATH:$HOME/dotnet:$HOME/.apt/usr/bin"
+export CORECLR_DIR="$HOME/dotnet/shared/Microsoft.NETCore.App/1.0.1"
 
 #DNX_BIN=`find .dnx/runtimes -type d -name bin`
 #export PATH="$PATH:/app/mono/bin:$DNX_BIN"
